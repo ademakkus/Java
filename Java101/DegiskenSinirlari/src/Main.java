afdea5d5d9d18408ca70816892349309830187bf
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-06-01 13:43:53 +0300

    Add files via upload

---
+++ Java101/DegiskenSinirlari/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("****JAVA Değiken Sınırları****");
+		System.out.println("Byte veri tpipi");
+		System.out.println("Byte üst sınır:"+Byte.MAX_VALUE);
+		System.out.println("Byte al sınır :"+Byte.MIN_VALUE);
+		System.out.println("------------------------------------");
+		System.out.println("Integer veri tpipi");
+		System.out.println("Integer üst sınır:"+Integer.MAX_VALUE);
+		System.out.println("Integer al sınır :"+Integer.MIN_VALUE);
+		System.out.println("------------------------------------");
+		System.out.println("Double veri tpipi");
+		System.out.println("Double üst sınır:"+Double.MAX_VALUE);
+		System.out.println("Double al sınır :"+Double.MIN_VALUE);
+		System.out.println("------------------------------------");
+		System.out.println("Float veri tpipi");
+		System.out.println("Float üst sınır:"+Float.MAX_VALUE);
+		System.out.println("Float al sınır :"+Float.MIN_VALUE);
+		System.out.println("------------------------------------");
+		System.out.println("Long veri tpipi");
+		System.out.println("Long üst sınır:"+Long.MAX_VALUE);
+		System.out.println("Long al sınır :"+Long.MIN_VALUE);
+		System.out.println("------------------------------------");
+		System.out.println("Short veri tpipi");
+		System.out.println("Short üst sınır:"+Short.MAX_VALUE);
+		System.out.println("Short al sınır :"+Short.MIN_VALUE);
+		System.out.println("------------------------------------");
+
+
+
+
+	}
+
+}
