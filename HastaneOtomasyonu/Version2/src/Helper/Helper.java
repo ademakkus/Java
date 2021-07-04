45daf12078d14f46ca1a4070c9889dcef90f7659
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2021-07-04 19:47:54 +0300

    Add files via upload

---
+++ HastaneOtomasyonu/Version2/src/Helper/Helper.java
@@ -0,0 +1,46 @@
+package Helper;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+public class Helper {
+public static void showMessage(String str) {
+	String msg;
+	optionPaneChangeButtonText(); 
+	switch(str) {
+	case "fill":
+		msg="Lütfen tüm alanları doldurunuz!";break;
+		
+	case "success":
+		msg="İşlem başarılı!";break;
+	default:
+			msg=str;
+		
+	}
+	JOptionPane.showMessageDialog(null,msg,"Uyarı Mesajı", JOptionPane.INFORMATION_MESSAGE);
+	optionPaneChangeButtonText(); 
+}
+public static boolean confirm(String str) {
+String msg;
+optionPaneChangeButtonText(); 
+switch(str) {
+case "sure":msg="Bu işlemi gerçekleştirmek istiyor musunuz?";break;
+default:msg=str;break;
+
+}
+int res=JOptionPane.showConfirmDialog(null, msg, "Dikkat !", JOptionPane.YES_NO_OPTION);
+optionPaneChangeButtonText(); 
+if(res==0)
+	return true;
+else
+	return false;
+}
+public static void optionPaneChangeButtonText() {
+	UIManager.put("OptionOPane.cancelButtonText", "İptal");
+	UIManager.put("OptionOPane.noButtonText", "Hayır");
+	UIManager.put("OptionOPane.okButtonText", "Tamam");
+	UIManager.put("OptionOPane.yesButtonText", "Evet");
+}
+
+}
+
---
+++ HastaneOtomasyonu/Version2/src/Model/User.java
@@ -0,0 +1,57 @@
+package Model;
+
+import java.sql.Connection;
+
+import Helper.DBConnection;
+
+public class User {
+	DBConnection conn=new DBConnection();
+	private String tcno;
+	private String name;
+	private String password;
+	private String type;
+public User(int id, String tcno, String name, String password, String type) {
+		super();
+		this.id = id;
+		this.tcno = tcno;
+		this.name = name;
+		this.password = password;
+		this.type = type;
+		
+	}
+public User() {
+		super();
+	}
+private int id;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTcno() {
+	return tcno;
+}
+public void setTcno(String tcno) {
+	this.tcno = tcno;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+
+}
